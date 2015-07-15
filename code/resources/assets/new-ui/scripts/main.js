b88b352af60e07d5ec12c2708508262c522f315d
Robin Poort <robin@timble.net>
2015-07-15 16:30:59 +0200

    initially moving from ui repo to this branch

---
+++ code/resources/assets/new-ui/scripts/main.js
@@ -0,0 +1,213 @@
+$(document).ready(function() {
+
+
+    // Sticky table header and footer
+    // @TODO: find a better one that's actually responsive
+   // $('.table--fixed').fixedHeaderTable({ footer: true, cloneHeadToFoot: false, fixedColumn: false, height: "auto" });
+
+    $(document).ready(function() {
+        $('.table--fixed').tableHeadFixer({'foot' : true, 'head' : true});
+    });
+    
+
+    // Enable Select2 for all selectboxes within a select2 class container
+    var select2 = $('.select2');
+    if ( select2 ) {
+        select2.select2();
+    }
+
+    // Variables
+    var koowa = $('#koowa'),
+        sidebar = $('.k-sidebar'),
+        component = $('.k-component'),
+        toolbar = $('.k-toolbar'),
+        togglebutton = $('.k-button--toggle'),
+        tableitems = $('tbody td'),
+        dragbar = $('.dragbar'),
+        toggleclass = 'k-toggle',
+        openedclass = 'k-opened',
+        closedclass = 'k-closed',
+        activeclass = 'k-active',
+        zindex = koowa.css('z-index');
+
+
+    // Table items
+    tableitems.click(function() {
+        var parent = $(this).parent();
+        var checkbox = parent.find('input');
+        parent.toggleClass(activeclass);
+        if ( checkbox.is(':checked') ) {
+            checkbox.prop('checked', false);
+        } else {
+            checkbox.prop('checked', true);
+        }
+    });
+
+
+    function openSidebar() {
+        sidebar.removeClass(closedclass).addClass(openedclass);
+        $('.collapsible').show();
+    }
+
+    function closeSidebar() {
+        sidebar.addClass(closedclass).removeClass(openedclass);
+        $('.collapsible').hide();
+        $('.k-tree__items').hide();
+    }
+
+    function resetSidebar() {
+        sidebar.removeClass(closedclass).removeClass(openedclass);
+        $('.collapsible').show();
+    }
+
+    function openComponent() {
+        component.removeClass(closedclass).addClass(openedclass);
+    }
+
+    function closeComponent() {
+        component.addClass(closedclass).removeClass(openedclass);
+    }
+
+    function resetComponent() {
+        component.removeClass(closedclass).removeClass(openedclass);
+    }
+
+    function toggleToolbar() {
+        toolbar.addClass(toggleclass);
+    }
+
+    function resetToolbar() {
+        toolbar.removeClass(toggleclass);
+    }
+
+    function toggleTree(element) {
+        element.toggleClass('toggled').next('ul').toggleClass('opened').slideToggle('fast');
+    }
+
+    function toggleSidebar(setting, zindex) {
+        if ( setting == 'resize' ) {
+            if ( zindex == 0 ) {
+                closeSidebar();
+            } else {
+                resetSidebar();
+                resetComponent();
+                resetToolbar();
+            }
+        }
+        if ( setting == 'toggle' || setting == 'tree' ) {
+            if ( zindex == 0 ) {
+                if ( component.hasClass(closedclass) ) {
+                    openComponent();
+                    closeSidebar();
+                } else {
+                    closeComponent();
+                    openSidebar();
+                    if ( setting == 'tree' ) {
+                        toggleTree( $('.k-tree > a') );
+                    }
+                }
+            } else {
+                if ( sidebar.hasClass(closedclass) ) {
+                    resetSidebar();
+                    resetComponent();
+                    resetToolbar();
+                    if ( setting == 'tree' ) {
+                        toggleTree( $('.k-tree > a') );
+                    }
+                } else if ( setting != 'tree' ) {
+                    closeSidebar();
+                    toggleToolbar();
+                }
+            }
+        }
+    }
+
+    // Clicking the main toggle button
+    if ( togglebutton.length ) {
+        togglebutton.click(function(event) {
+            event.preventDefault();
+            zindex = koowa.css('z-index');
+            toggleSidebar('toggle', zindex);
+        });
+    }
+
+    // Clicking the parent item of the tree
+    $('.k-tree > a').click(function(event) {
+        event.preventDefault();
+        zindex = koowa.css('z-index');
+        toggleSidebar('tree', zindex);
+    });
+
+
+    // When window gets resized
+    $(window).on('resize', function() {
+        var oldzindex = zindex;
+        zindex = koowa.css('z-index');
+        if ( zindex != oldzindex ) {
+            toggleSidebar('resize', zindex);
+        }
+    });
+
+
+    // The tree
+    $('.k-clicker').click(function(event) {
+        event.stopPropagation();
+        var element = $(this).parent();
+        toggleTree(element);
+    });
+
+
+    $('.k-sidebar__item').overflowing();
+
+
+    // Magnific popup
+    $('.mfp-iframe').magnificPopup({
+        type: 'iframe',
+        mainClass: 'koowa_dialog_modal'
+    });
+
+
+    // Repaint
+    function repaint(element) {
+        setTimeout(function() {
+            element.hide(0).show(0);
+        }, 100);
+    }
+
+    // Temp template switcher to test
+    $(document).bind('keydown', function(e) {
+        var csslink = $('link[href*="admin"]');
+        var body = $('body');
+        // 2
+        if (e.which == 50) {
+            csslink.attr('href', 'css/admin-default.css');
+            repaint(body);
+        }
+        // 3
+        if (e.which == 51) {
+            csslink.attr('href', 'css/admin-other.css');
+            repaint(body);
+        }
+        // 4
+        if (e.which == 52) {
+            csslink.attr('href', 'css/admin-joomla.css');
+            repaint(body);
+        }
+        // 5
+        if (e.which == 53) {
+            csslink.attr('href', 'css/admin-wordpress.css');
+            repaint(body);
+        }
+        // F
+        if (e.which == 70) {
+            $('.k-overview').toggleClass('k-flex');
+            repaint(body);
+        }
+        // J
+        if (e.which == 74) {
+            koowa.toggleClass('js-enabled');
+            repaint(body);
+        }
+    });
+
+});
---
+++ gruntfile.js
@@ -0,0 +1,152 @@
+module.exports = function(grunt) {
+
+    // measures the time each task takes
+    require('time-grunt')(grunt);
+
+    // load time-grunt and all grunt plugins found in the package.json
+    require('jit-grunt')(grunt);
+
+    // grunt config
+    grunt.initConfig({
+
+        // Iconfont
+        webfont: {
+            icons: {
+                src: 'code/resources/assets/new-ui/icons/svg/*.svg',
+                dest: 'code/resources/assets/new-ui/fonts/icons',
+                destCss: 'code/resources/assets/new-ui/scss/utilities',
+                options: {
+                    font: 'koowa-icons',
+                    hashes: false,
+                    stylesheet: 'scss',
+                    relativeFontPath: '../fonts/icons/',
+                    template: 'code/resources/assets/new-ui/icons/template.css',
+                    htmlDemo: false
+                }
+            }
+        },
+
+
+        // Compile sass files
+        sass: {
+            options: {
+                //outputStyle: 'compressed'
+            },
+            dist: {
+                files: {
+                    'code/resources/assets/new-ui/css/admin-default.css': 'code/resources/assets/new-ui/scss/admin-default.scss',
+                    'code/resources/assets/new-ui/css/admin-other.css': 'code/resources/assets/new-ui/scss/admin-other.scss',
+                    'code/resources/assets/new-ui/css/admin-joomla.css': 'code/resources/assets/new-ui/scss/admin-joomla.scss',
+                    'code/resources/assets/new-ui/css/admin-wordpress.css': 'code/resources/assets/new-ui/scss/admin-wordpress.scss'
+                }
+            }
+        },
+
+
+        // Browser Sync
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : ["code/resources/assets/new-ui/*.*"]
+                },
+                options: {
+                    proxy: "todo.dev",
+                    port: 1338,
+                    open: true,
+                    notify: false,
+                    watchTask: true,
+                    injectChanges: false
+                }
+            }
+        },
+
+
+        // Uglify
+        uglify: {
+            options: {
+                soureMap: true
+            },
+            build: {
+                files: {
+                    'code/resources/assets/new-ui/js/scripts.js': [
+                        'bower_components/jquery/dist/jquery.min.js',
+                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+                        'bower_components/select2/dist/js/select2.min.js',
+                        'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
+                        'code/resources/assets/new-ui/scripts/tableHeadFixer.js',
+                        'code/resources/assets/new-ui/scripts/overflowing.js',
+                        'code/resources/assets/new-ui/scripts/main.js'
+                    ],
+                    '_jekyll/js/modernizr.js': [
+                        'bower_components/modernizr/modernizr.js'
+                    ]
+                }
+            }
+        },
+
+
+        // Autoprefixer
+        autoprefixer: {
+            options: {
+                browsers: ['> 5%', 'last 2 versions', 'ie 11', 'ie 10', 'ie 9']
+            },
+            files: {
+                expand: true,
+                flatten: true,
+                src: 'code/resources/assets/new-ui/css/*.css',
+                dest: 'code/resources/assets/new-ui/css/'
+            }
+        },
+
+
+        // Shell commands
+        shell: {
+            updateCanIUse: {
+                command: 'npm update caniuse-db'
+            }
+        },
+
+
+        // Watch files
+        watch: {
+            fontcustom: {
+                files: [
+                    'code/resources/assets/new-ui/icons/svg/*.svg'
+                ],
+                tasks: ['webfont', 'sass', 'autoprefixer'],
+                options: {
+                    interrupt: true,
+                    atBegin: false
+                }
+            },
+            sass: {
+                files: [
+                    'code/resources/assets/new-ui/scss/*.scss',
+                    'code/resources/assets/new-ui/scss/**/*.scss'
+                ],
+                tasks: ['sass', 'autoprefixer'],
+                options: {
+                    interrupt: true,
+                    atBegin: true
+                }
+            },
+            uglify: {
+                files: [
+                    // Including
+                    'code/resources/assets/new-ui/scripts/*.js'
+                ],
+                tasks: ['uglify'],
+                options: {
+                    interrupt: true,
+                    atBegin: false
+                }
+            }
+        }
+
+
+    });
+
+    // The dev task will be used during development
+    grunt.registerTask('default', ['shell', 'browserSync', 'watch']);
+
+};
