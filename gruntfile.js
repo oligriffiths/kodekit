004963769fff5914b4101716b1d6b31c2844547f
Robin Poort <robin@timble.net>
2015-07-16 16:03:19 +0200

    re #227 - cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -74,7 +74,6 @@ module.exports = function(grunt) {
                 files: {
                     'code/resources/assets/js/scripts.js': [
                         'bower_components/jquery/dist/jquery.min.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
                         'bower_components/select2/dist/js/select2.min.js',
                         'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
                         'code/resources/assets/new-ui/scripts/tableHeadFixer.js',
