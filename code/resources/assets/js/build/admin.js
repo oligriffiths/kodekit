6fcfbcb5323ee5e7cd27bd690e96c73e70b2b320
Ercan Ozkaya <ercan@timble.net>
2015-10-02 13:24:11 +0300

    re #227: Cleanup

--- code/resources/assets/js/build/admin.js
+++ code/resources/assets/js/build/admin.js
@@ -617,13 +617,9 @@ b.id!=f.id&&d.push(f.id)}c.$element.val(d.join(c._valueSeparator)),c.$element.tr
 
         // Variables
         var $wrapper = $('.k-overview'),
-            $sidebar = $('#k-sidebar'),
-            $toolbar = $sidebar.parent().find('.k-toolbar'),
-            $sidebarright = $('#k-sidebar-right'),
-            $sidebaritem = $('.k-sidebar__item--overflow'),
+            $toolbar = $('.k-toolbar'),
             $content = $('.k-content'),
             $fixedtable = $('.table--fixed'),
-            $tableitems = $('tbody td'),
             $select2default = $('.select2--default'),
             $select2filter = $('.select2--filter select'),
             $tree = $('.k-clicker'),
@@ -631,42 +627,43 @@ b.id!=f.id&&d.push(f.id)}c.$element.val(d.join(c._valueSeparator)),c.$element.tr
             $clickable = $('a, button'),
             $searchtoggle = $('.toggle-search');
 
-        var toggle_button = '<button class="off-canvas-menu-toggle" type="button">' +
-            '<span class="bar1"></span>' +
-            '<span class="bar2"></span>' +
-            '<span class="bar3"></span>' +
-            '</button>';
+        // Sidebar
+        if ($wrapper.length && $content.length && $toolbar.length)
+        {
+            var toggle_button = '<button class="off-canvas-menu-toggle" type="button">' +
+                '<span class="bar1"></span>' +
+                '<span class="bar2"></span>' +
+                '<span class="bar3"></span>' +
+                '</button>',
+                sidebar_left  = $('#k-sidebar'),
+                sidebar_right = $('#k-sidebar-right');
 
+            if (sidebar_left.length) {
+                var left_toggle = $(toggle_button);
+                $toolbar.prepend(left_toggle);
 
-        // Offcanvasmenu
-        if ( $sidebar.length && $wrapper.length && $content.length && $toolbar.length ) {
-            var left_toggle = $(toggle_button);
-            $toolbar.prepend(left_toggle);
+                sidebar_left.offCanvasMenu({
+                    menuToggle: left_toggle,
+                    wrapper: $wrapper,
+                    container: $content
+                });
+            }
 
-            $sidebar.offCanvasMenu({
-                menuToggle: left_toggle,
-                wrapper: $wrapper,
-                container: $content
-            });
+            if (sidebar_right.length) {
+                var right_toggle = $(toggle_button);
+                $toolbar.append(right_toggle);
+
+                sidebar_right.offCanvasMenu({
+                    menuToggle: right_toggle,
+                    wrapper: $wrapper,
+                    container: $content,
+                    position: 'right'
+                });
+            }
+
+            // Overflowing sidebar items
+            $('.k-sidebar__item--overflow').overflowing();
         }
-
-        // Offcanvasmenu
-        if ( $sidebarright.length && $wrapper.length && $content.length && $toolbar.length) {
-            var right_toggle = $(toggle_button);
-            $toolbar.append(right_toggle);
-
-            $sidebarright.offCanvasMenu({
-                menuToggle: right_toggle,
-                wrapper: $wrapper,
-                container: $content,
-                position: 'right'
-            });
-        }
-
-
-
-
-
 
         // Footable
         $('.footable').footable({
@@ -680,6 +677,11 @@ b.id!=f.id&&d.push(f.id)}c.$element.val(d.join(c._valueSeparator)),c.$element.tr
             fixedTable();
         });
 
+        // WP sidebar toggle
+        $('#collapse-menu').on('click', function() {
+            $fixedtable.floatThead('destroy');
+            fixedTable();
+        });
 
         // Sticky table header and footer
         function fixedTable() {
@@ -692,8 +694,8 @@ b.id!=f.id&&d.push(f.id)}c.$element.val(d.join(c._valueSeparator)),c.$element.tr
                 });
             }
         }
-        fixedTable();
 
+        fixedTable();
 
         // Clickable items
         if ( $clickable.length ) {
@@ -701,7 +703,6 @@ b.id!=f.id&&d.push(f.id)}c.$element.val(d.join(c._valueSeparator)),c.$element.tr
                 $(this).toggleClass('k-opened');
             });
         }
-
 
         // Enable Select2 for all selectboxes within a select2 class container
         if ( $select2default.length ) {
@@ -726,13 +727,6 @@ b.id!=f.id&&d.push(f.id)}c.$element.val(d.join(c._valueSeparator)),c.$element.tr
             });
         }
 
-
-        // Overflowing sidebar items
-        if ( $sidebaritem.length ) {
-            $sidebaritem.overflowing();
-        }
-
-
         // Magnific popup
         if ( $magnificframe.length ) {
             $magnificframe.magnificPopup({
@@ -741,19 +735,10 @@ b.id!=f.id&&d.push(f.id)}c.$element.val(d.join(c._valueSeparator)),c.$element.tr
             });
         }
 
-
         // Toggle search
         $searchtoggle.click(function() {
             $('.k-scopebar__search').slideToggle('fast');
         });
-
-        // WP sidebar toggle
-        $('#collapse-menu').on('click', function() {
-            $fixedtable.floatThead('destroy');
-            fixedTable();
-        });
-
-
     });
 
 })(jQuery);
--- code/resources/assets/scripts/main.js
+++ code/resources/assets/scripts/main.js
@@ -4,13 +4,9 @@
 
         // Variables
         var $wrapper = $('.k-overview'),
-            $sidebar = $('#k-sidebar'),
-            $toolbar = $sidebar.parent().find('.k-toolbar'),
-            $sidebarright = $('#k-sidebar-right'),
-            $sidebaritem = $('.k-sidebar__item--overflow'),
+            $toolbar = $('.k-toolbar'),
             $content = $('.k-content'),
             $fixedtable = $('.table--fixed'),
-            $tableitems = $('tbody td'),
             $select2default = $('.select2--default'),
             $select2filter = $('.select2--filter select'),
             $tree = $('.k-clicker'),
@@ -18,42 +14,43 @@
             $clickable = $('a, button'),
             $searchtoggle = $('.toggle-search');
 
-        var toggle_button = '<button class="off-canvas-menu-toggle" type="button">' +
-            '<span class="bar1"></span>' +
-            '<span class="bar2"></span>' +
-            '<span class="bar3"></span>' +
-            '</button>';
+        // Sidebar
+        if ($wrapper.length && $content.length && $toolbar.length)
+        {
+            var toggle_button = '<button class="off-canvas-menu-toggle" type="button">' +
+                '<span class="bar1"></span>' +
+                '<span class="bar2"></span>' +
+                '<span class="bar3"></span>' +
+                '</button>',
+                sidebar_left  = $('#k-sidebar'),
+                sidebar_right = $('#k-sidebar-right');
 
+            if (sidebar_left.length) {
+                var left_toggle = $(toggle_button);
+                $toolbar.prepend(left_toggle);
 
-        // Offcanvasmenu
-        if ( $sidebar.length && $wrapper.length && $content.length && $toolbar.length ) {
-            var left_toggle = $(toggle_button);
-            $toolbar.prepend(left_toggle);
+                sidebar_left.offCanvasMenu({
+                    menuToggle: left_toggle,
+                    wrapper: $wrapper,
+                    container: $content
+                });
+            }
 
-            $sidebar.offCanvasMenu({
-                menuToggle: left_toggle,
-                wrapper: $wrapper,
-                container: $content
-            });
+            if (sidebar_right.length) {
+                var right_toggle = $(toggle_button);
+                $toolbar.append(right_toggle);
+
+                sidebar_right.offCanvasMenu({
+                    menuToggle: right_toggle,
+                    wrapper: $wrapper,
+                    container: $content,
+                    position: 'right'
+                });
+            }
+
+            // Overflowing sidebar items
+            $('.k-sidebar__item--overflow').overflowing();
         }
-
-        // Offcanvasmenu
-        if ( $sidebarright.length && $wrapper.length && $content.length && $toolbar.length) {
-            var right_toggle = $(toggle_button);
-            $toolbar.append(right_toggle);
-
-            $sidebarright.offCanvasMenu({
-                menuToggle: right_toggle,
-                wrapper: $wrapper,
-                container: $content,
-                position: 'right'
-            });
-        }
-
-
-
-
-
 
         // Footable
         $('.footable').footable({
@@ -67,6 +64,11 @@
             fixedTable();
         });
 
+        // WP sidebar toggle
+        $('#collapse-menu').on('click', function() {
+            $fixedtable.floatThead('destroy');
+            fixedTable();
+        });
 
         // Sticky table header and footer
         function fixedTable() {
@@ -79,8 +81,8 @@
                 });
             }
         }
-        fixedTable();
 
+        fixedTable();
 
         // Clickable items
         if ( $clickable.length ) {
@@ -88,7 +90,6 @@
                 $(this).toggleClass('k-opened');
             });
         }
-
 
         // Enable Select2 for all selectboxes within a select2 class container
         if ( $select2default.length ) {
@@ -113,13 +114,6 @@
             });
         }
 
-
-        // Overflowing sidebar items
-        if ( $sidebaritem.length ) {
-            $sidebaritem.overflowing();
-        }
-
-
         // Magnific popup
         if ( $magnificframe.length ) {
             $magnificframe.magnificPopup({
@@ -128,19 +122,10 @@
             });
         }
 
-
         // Toggle search
         $searchtoggle.click(function() {
             $('.k-scopebar__search').slideToggle('fast');
         });
-
-        // WP sidebar toggle
-        $('#collapse-menu').on('click', function() {
-            $fixedtable.floatThead('destroy');
-            fixedTable();
-        });
-
-
     });
 
 })(jQuery);
